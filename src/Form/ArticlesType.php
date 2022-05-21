59a8e0f8e81c8404a71458a0a7e52b3cbce4eb02
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-05-21 07:12:28 +0200

    Create ArticlesType.php

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use App\Entity\Categories;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', CKEditorType::class)
+            ->add('image', TextType::class)
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class
+                ])
+            ->add('author', TextType::class)
+            ->add('VALIDER', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
