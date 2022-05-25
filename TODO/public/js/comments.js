187acabc16bfe2eff71efb474710a7d1a89b9572
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-05-25 17:04:58 +0200

    Create comments.js

---
+++ TODO/public/js/comments.js
@@ -0,0 +1,7 @@
+// document.querySelector("#article_contact_title").value = "{{ article.title|raw }}";
+
+// document.querySelectorAll("[data-commentair]").forEach(element => {
+//     element.addEventListener("click", function() {
+//         console.log(this);
+//     });
+// });
